be7f92eedaca3333780f0ad6a8b8747919ac439b
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-07-05 19:56:44 -0300

    Add files ordenation names

---
+++ Resolvendo_problemas/ordanition_name.rb
@@ -0,0 +1,8 @@
+def name (arr)
+    ordenation = arr.sort
+end
+
+lista = ['Carla', 'José', 'Maria', 'Leandro', 'Pablo', 'Ana']
+
+result = name(lista)
+puts "Está é minha lista ordenada alfabética #{result}"
