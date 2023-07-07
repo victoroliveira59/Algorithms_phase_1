51ce82a3647b5271e9a4b6e484dcd02b5be4f532
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-07-07 06:08:10 -0300

    Add files counting_sort

---
+++ Resolvendo_problemas/counting_sort.rb
@@ -0,0 +1,28 @@
+def counting_sort (arr)
+
+    max_value = arr.length
+
+    count = Array.new(max_value -1,0)
+
+    arr_b = Array.new(0)
+
+
+    arr.each do |element| 
+        count[element - 1] += 1 
+    end
+
+    count.each.with_index do | element, i|
+        count[i] = count[i] + count[i-1]              #Esta linha representa a soma do meu indice que já está armazenado mais o indice anterior representado por:c[i] = c[i] + c[i-1]
+    end 
+    (arr.length - 1).downto (0) do |i|
+        arr_b[count[arr[i]-1]-1] = arr[i]
+            count[arr[i]-1] -= 1  
+    end
+    return arr_b
+end
+
+lista = [3,5,6,7,7,6,3,2,1,4]
+
+result = counting_sort(lista)
+
+puts "O resultado do coutintg sort é #{result}"
