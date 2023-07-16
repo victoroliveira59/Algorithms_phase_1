5dd1d85d0d43a580a7c3156816f4be176ec44f9a
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-07-16 11:51:13 -0300

    syntax replacement in name list

--- Resolvendo_problemas/ordanition_name.rb
+++ Resolvendo_problemas/ordanition_name.rb
@@ -1,8 +1,8 @@
 def name (arr)
-    ordenation = arr.sort
+    arr.sort
 end
 
-lista = ['Carla', 'José', 'Maria', 'Leandro', 'Pablo', 'Ana']
+lista = %w[Carla José Maria Leandro Pablo Ana]
 
 result = name(lista)
 puts "Está é minha lista ordenada alfabética #{result}"
