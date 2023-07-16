74558c96c74d7f4680bb30ff173741ebc1785c61
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-07-16 11:53:33 -0300

    defining a function for the max_value variable, deleting return and modifications for more readable code

--- Resolvendo_problemas/counting_sort.rb
+++ Resolvendo_problemas/counting_sort.rb
@@ -1,24 +1,28 @@
-def counting_sort (arr)
+#noinspection RubyUnusedLocalVariable
+def max_value(i, i1)
+    # code here
+end
 
-    max_value = arr.length
+#noinspection RubyArgumentParentheses
+def counting_sort (arr)
 
     count = Array.new(max_value -1,0)
 
-    arr_b = Array.new(0)
+    arr_b = [0]
 
 
     arr.each do |element| 
         count[element - 1] += 1 
     end
 
-    count.each.with_index do | element, i|
+    count.each.with_index do | _, i|
         count[i] = count[i] + count[i-1]              #Esta linha representa a soma do meu indice que já está armazenado mais o indice anterior representado por:c[i] = c[i] + c[i-1]
     end 
     (arr.length - 1).downto (0) do |i|
         arr_b[count[arr[i]-1]-1] = arr[i]
             count[arr[i]-1] -= 1  
     end
-    return arr_b # Essa função retorna o valor do meu novo array(arr_b) para subscrever sobre o array principal
+    arr_b # Essa função retorna o valor do meu novo array(arr_b) para subscrever sobre o array principal
 end
 
 lista = [3,5,6,7,7,6,3,2,1,4]
