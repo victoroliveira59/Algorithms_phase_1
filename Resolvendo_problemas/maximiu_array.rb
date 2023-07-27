2b022c6f039f2560d61a6defa3e43a9001a2b4a7
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-07-26 21:22:09 -0300

    Update files

--- Resolvendo_problemas/maximiu_array.rb
+++ Resolvendo_problemas/maximiu_array.rb
@@ -1,22 +1,22 @@
-arr = [ 9,8,8,5 ]
+arr = [9,-8,8,3]
 
 a = arr.sort
-k = 4
+k = 3
 
 a.each_with_index do |value, i|
-  if k > 0
-    if value < 0
-      a[i] *= - 1
-      k -= 1
-    elsif value == 0
-      k = 0
-      break
-    elsif value > 0
-      a[i] *= - 1
-      break
-    end
-  else 
-    break
-  end
+	if k > 0
+		if value < 0
+			a[i] *= - 1
+			k -= 1
+		elsif value == 0
+			k = 0
+			break
+		elsif value > 0
+			a[i] *= - 1
+			break
+		else
+			break
+		end
+	end
 end
 puts a.sum
