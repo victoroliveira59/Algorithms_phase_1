e74ee034a641ee42d1fdf486ab5d0136ce8d7bb0
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-08-07 20:57:14 -0300

    Update files

--- Resolvendo_problemas/debug_codigos.rb
+++ Resolvendo_problemas/debug_codigos.rb
@@ -1,6 +1,6 @@
 require 'json'
 
-pasta_codigo = "/home/victoroliveira59/Documentos/Github/Ruby/Algoritmos_fase_1/Resolvendo_problemas"
+pasta_codigo = "/home/victoroliveira59/Documentos/Github/Ruby/Desenvolvendo_me_/Resolvendo_problemas"
 
 # Percorre todos os arquivos da pasta "Código"
 Dir.foreach(pasta_codigo) do |nome_arquivo|
