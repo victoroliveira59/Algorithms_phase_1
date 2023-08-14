b27e0350bbcb8f834fb7cc2af1c9785da6ea753e
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-08-14 20:25:55 -0300

    Update files

--- Algoritimos_fase1/bubble_sort.rb
+++ Algoritimos_fase1/bubble_sort.rb
@@ -4,11 +4,11 @@ class Bublle_sort
 		@arr = arr
 	end
 
-	def bubbe_sort(arr)
+	def bubble_sort(arr)
 		arr.each_index do |i|
 			max_index = arr[i..-1].each_with_index.min_by { |num, _| num }[1] + i
 			arr[i], arr[max_index] = arr[max_index], arr[i]
-			swapped = true
+			true
 		end
 		arr
 	end
@@ -16,6 +16,6 @@ end
 
 lista = [9,2,6,3,7,1,-3,-8]
 result = Bublle_sort.new
-print result.bubbe_sort(lista)
+print result.bubble_sort(lista)
 
 # puts "Minha lista ficou ordenada da seguinte forma: #{result.inspect}"
--- spec/lib/Algoritimos_fase1/bubble_sort_spec.rb
+++ spec/lib/Algoritimos_fase1/bubble_sort_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe Bublle_sort do
 			unsorted_list = [9, 2, 6, 3, 7, 1, -3, -8]
 			sorted_list = [-8, -3, 1, 2, 3, 6, 7, 9]
 			instance = Bublle_sort.new
-			expect(instance.bubbe_sort(unsorted_list)).to eq(sorted_list)
+			expect(instance.bubble_sort(unsorted_list)).to eq(sorted_list)
 		end
 
 		it 'handles an already sorted list' do
 			sorted_list = [-8, -3, 1, 2, 3, 6, 7, 9]
 			instance = Bublle_sort.new
-			expect(instance.bubbe_sort(sorted_list)).to eq(sorted_list)
+			expect(instance.bubble_sort(sorted_list)).to eq(sorted_list)
 		end
 	end
 end
