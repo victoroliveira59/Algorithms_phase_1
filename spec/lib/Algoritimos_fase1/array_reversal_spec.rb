6865b2cd15d2eed1ab38b5ecbeb47976c6357be8
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-08-15 05:10:37 -0300

    Add file testing array reverse

--- spec/lib/Algoritimos_fase1/array_reversal_spec.rb
+++ spec/lib/Algoritimos_fase1/array_reversal_spec.rb
@@ -1,10 +1,15 @@
-require 'spec_helper'
-require './Resolvendo_problemas/array_reversal.rb'
+require './spec/spec_helper'
+require './Algoritimos_fase1/array_reversal.rb'
 
-describe array_reversal do
+RSpec.describe 'reversal and rothate' do
 	context "testing algorithms fase 01" do
-		it "testing algorithm reversal" do
-			expect(array_reversal[1,2,3,4,5,6,7,8,9,10].to eq ['8,9,10,1,2,3,4,5,6,7'])
+		it "change of numbers rothate" do
+			arr = [1,2,3,4,5,6,7,8,9,10]
+			d = 3
+			n = arr.size
+			output = [8,9,10,1,2,3,4,5,6,7]
+			rothate(arr, d ,n)
+			expect(arr).to eq(output)
 		end
 	end
 end
