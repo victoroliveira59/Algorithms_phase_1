82a0bb7bf61ebcd3703f006836e0b757a63cb93b
victoroliveira59 <victorsamuelolveira59@gmail.com>
2023-09-01 05:26:12 -0300

    test: rotation tests

--- spec/lib/Algoritimos_fase1/array_reversal_spec.rb
+++ spec/lib/Algoritimos_fase1/array_reversal_spec.rb
@@ -3,22 +3,19 @@ require './Algoritimos_fase1/array_reversal.rb'
 
 RSpec.describe 'reversal and rothate' do
 	context "testing algorithms fase 01" do
-		it "change of numbers rothate" do
-			arr = [1,2,3,4,5,6,7,8,9,10]
-			d = 3
-			n = arr.size
-			output = [8,9,10,1,2,3,4,5,6,7]
-			rothate(arr, d ,n)
-			expect(arr).to eq(output)
+		it "If the number of attempts is greater than the number of elements, it returns the original array" do
+			arr = [1,2,3,4,5,6,7,8,9]
+			rothate(arr, 12, arr.size)
+			expect(arr).to eq([1,2,3,4,5,6,7,8,9])
 		end
+
 		it "change of numbers rothate" do
 			arr = [1,2,3,4,5,6,7,8,9,10]
-			d = 5
-			n = arr.size
 			output = [6,7,8,9,10,1,2,3,4,5]
-			rothate(arr, d ,n)
+			rothate(arr, 5 ,arr.size)
 			expect(arr).to eq(output)
 		end
+
 		it "change of numbers rothate" do
 			arr = [10,9,8,7,6,5,4,3,2,1]
 			d = 6
