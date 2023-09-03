1d0e6b7959a17ac05f369333b4ac0ed45272b627
victoroliveira59 <victorsamuelolveira59@gmail.com>
2023-09-03 15:51:28 -0300

    Revert "test: greedy_algorithm"
    This reverts commit 50c5adaa95d8f4c77c083337264c4ada342713d6.

--- spec/lib/Algoritimos_fase1/greedy_algorithm_spec.rb
+++ spec/lib/Algoritimos_fase1/greedy_algorithm_spec.rb
@@ -3,7 +3,7 @@ require './Algoritimos_fase1/greedy_algorithm.rb'
 
 RSpec.describe 'greedy' do
 	context 'Testing algorithm greedy' do
-		it 'If the amount paid is greater' do
+		it 'Searching the algorithm greedy' do
 			input = 200
 			received = 75
 			expected = [50,50,25]
@@ -11,7 +11,6 @@ RSpec.describe 'greedy' do
 			result = greedy(input, received)
 			expect(result).to eq(expected)
 		end
-
 		it 'If the amount received is equal to the amount paid' do
 			input = 200
 			received = 200
@@ -22,6 +21,5 @@ RSpec.describe 'greedy' do
 		end
 	end
 end
-
 
 
